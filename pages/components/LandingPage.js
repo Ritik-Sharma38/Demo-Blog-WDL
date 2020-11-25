a57123209d4027725ed837bbb6718d52ca166598
shivanishimpi <shivanishimpi9@gmail.com>
2020-11-25 23:07:32 +0530

    button-spacing

--- pages/components/LandingPage.js
+++ pages/components/LandingPage.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Button } from './Button';
-import styles from '../../styles/Home.module.css'
+import React from "react";
+import { Button } from "./Button";
+import styles from "../../styles/Home.module.css";
 
 function LandingPage() {
   return (
     <div className={styles.heroContainer}>
-      <video className={styles.video} src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>MoonLight Sunshine</h1>
-      <p>I've been a fool !</p>
-      <div className={styles.heroBtns}>
-        <Button
-          className={styles.btn}
-          buttonStyle={styles.btnOutline}
-          buttonSize={styles.btnLarge}
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className={styles.btn}
-          buttonStyle={styles.btnPrimary}
-          buttonSize={styles.btnLarge}
-        >
-          WATCH STORIES
-        </Button>
+      <video
+        className={styles.video}
+        src="/videos/video-1.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className={styles.sub_heroContainer}>
+        <h1>MoonLight Sunshine</h1>
+        <p>I've been a fool!</p>
+      </div>
+      <div className={styles.mainbutton}>
+        <div className={styles.leftbutton}>
+          <Button>GET STARTED</Button>
+        </div>
+        <div className={styles.rightbutton}>
+          <Button>WATCH STORIES</Button>
+        </div>
       </div>
     </div>
   );
